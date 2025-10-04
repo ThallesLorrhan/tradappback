17f5f98ff38749c5babb304abbfde352e3af2bb8
Thalles Lorrhan <tahlles.lorrhan30@gmail.com>
2025-10-04 15:47:28 -0300

    changes in serializers 1.2

--- chapels/serializers.py
+++ chapels/serializers.py
@@ -1,6 +1,6 @@
-# chapels/serializers.py
 from rest_framework import serializers
 from .models import Chapel, Responsible, ChapelImage, Mass
+import json
 
 class MassSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,27 +20,40 @@ class ChapelImageSerializer(serializers.ModelSerializer):
 class ChapelSerializer(serializers.ModelSerializer):
     masses = MassSerializer(many=True)
     responsibles = ResponsibleSerializer(many=True)
-    images = ChapelImageSerializer(many=True)
+    images = ChapelImageSerializer(many=True, required=False)
 
     class Meta:
         model = Chapel
         fields = '__all__'
 
     def create(self, validated_data):
-        masses_data = validated_data.pop('masses', [])
-        responsibles_data = validated_data.pop('responsibles', [])
-        images_data = validated_data.pop('images', [])
+        request = self.context.get('request')
 
+        # Parse JSON enviado via FormData
+        masses_data = json.loads(request.data.get('masses', '[]')) if request else []
+        responsibles_data = json.loads(request.data.get('responsibles', '[]')) if request else []
+
+        # Pega os arquivos de imagens
+        images_files = request.FILES.getlist('images') if request else []
+
+        # Remove campos do validated_data para não dar conflito
+        validated_data.pop('masses', None)
+        validated_data.pop('responsibles', None)
+        validated_data.pop('images', None)
+
+        # Cria a capela
         chapel = Chapel.objects.create(**validated_data)
 
+        # Cria massas
         for mass in masses_data:
             Mass.objects.create(chapel=chapel, **mass)
 
+        # Cria responsáveis
         for responsible in responsibles_data:
             Responsible.objects.create(chapel=chapel, **responsible)
 
-        for image in images_data:
-            ChapelImage.objects.create(chapel=chapel, **image)
+        # Cria imagens
+        for image_file in images_files:
+            ChapelImage.objects.create(chapel=chapel, image=image_file)
 
         return chapel
-
