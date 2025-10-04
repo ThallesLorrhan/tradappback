f8f081bc49678ccf959f88d8aca763c5b5a6ba3b
Thalles Lorrhan <tahlles.lorrhan30@gmail.com>
2025-10-04 18:00:09 -0300

    changes in serializres 1.3

--- chapels/serializers.py
+++ chapels/serializers.py
@@ -1,9 +1,30 @@
 # serializers.py
 from rest_framework import serializers
 from .models import Chapel, Responsible, ChapelImage, Mass
-import json
+
+class MassSerializer(serializers.ModelSerializer):
+    day_of_week_display = serializers.CharField(source='get_day_of_week_display', read_only=True)
+    mass_type_display = serializers.CharField(source='get_mass_type_display', read_only=True)
+
+    class Meta:
+        model = Mass
+        fields = ['id', 'day_of_week', 'day_of_week_display', 'time', 'mass_type', 'mass_type_display', 'notes']
+
+class ResponsibleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Responsible
+        fields = ['id', 'name', 'role', 'phone', 'email']
+
+class ChapelImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChapelImage
+        fields = ['id', 'image']
 
 class ChapelSerializer(serializers.ModelSerializer):
+    masses = MassSerializer(many=True, read_only=True)
+    responsibles = ResponsibleSerializer(many=True, read_only=True)
+    images = ChapelImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Chapel
         fields = '__all__'
@@ -11,27 +32,25 @@ class ChapelSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         request = self.context.get('request')
 
-        # Pegando massas e responsáveis do request
         masses_data = request.data.get('masses', '[]')
         responsibles_data = request.data.get('responsibles', '[]')
 
-        # Se forem strings JSON, converte para lista
+        # Convertendo de JSON se necessário
+        import json
         if isinstance(masses_data, str):
             masses_data = json.loads(masses_data)
         if isinstance(responsibles_data, str):
             responsibles_data = json.loads(responsibles_data)
 
-        # Latitude e longitude (tratando arrays)
+        # Latitude e longitude
         lat = request.data.get('latitude')
         lon = request.data.get('longitude')
-
         try:
             if isinstance(lat, list):
                 lat = lat[0]
             validated_data['latitude'] = float(lat) if lat else None
         except (TypeError, ValueError):
             validated_data['latitude'] = None
-
         try:
             if isinstance(lon, list):
                 lon = lon[0]
@@ -39,23 +58,19 @@ class ChapelSerializer(serializers.ModelSerializer):
         except (TypeError, ValueError):
             validated_data['longitude'] = None
 
-        # Remove campos extras que não existem na model
+        # Remover campos extras
         validated_data.pop('masses', None)
         validated_data.pop('responsibles', None)
         validated_data.pop('images', None)
 
-        # Cria a capela
         chapel = Chapel.objects.create(**validated_data)
 
-        # Cria massas
         for mass in masses_data:
             Mass.objects.create(chapel=chapel, **mass)
 
-        # Cria responsáveis
         for responsible in responsibles_data:
             Responsible.objects.create(chapel=chapel, **responsible)
 
-        # Cria imagens
         for image_file in request.FILES.getlist('images'):
             ChapelImage.objects.create(chapel=chapel, image=image_file)
 
